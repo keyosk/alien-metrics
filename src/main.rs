63a69abb5e9813d76a0c37c493cb517b61d587fc
Cody Carlsen <keyosk@gmail.com>
2023-02-19 00:20:31 -0800

    tidy pass
    Signed-off-by: Cody Carlsen <keyosk@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,35 @@ use alien_metrics::{AlienClient, AlienError, Metrics};
 use axum::{extract::State, http::StatusCode, routing::get, Router, Server};
 use prometheus::TextEncoder;
 use std::sync::Arc;
-use tokio::signal;
+use tokio::{
+    select,
+    signal::{
+        ctrl_c,
+        unix::{signal, SignalKind},
+    },
+    time::{sleep, Duration},
+};
+
+async fn main_loop(metrics: Arc<Metrics>) -> Result<(), AlienError> {
+    let one_sec = Duration::from_secs(1);
+    let sleep_interval = Duration::from_secs(15);
+
+    let mut alien_client = AlienClient::new().await?;
+
+    loop {
+        metrics.scrape_counter.inc();
+
+        let device_info = alien_client.get_info().await;
+
+        if let Ok(device_info) = device_info {
+            alien_client.record_metrics(&metrics, device_info)?;
+            sleep(sleep_interval).await
+        } else {
+            alien_client.re_login().await?;
+            sleep(one_sec).await
+        }
+    }
+}
 
 async fn serve_req(State(metrics): State<Arc<Metrics>>) -> (StatusCode, String) {
     let encoder = TextEncoder::new();
@@ -16,37 +44,14 @@ async fn serve_req(State(metrics): State<Arc<Metrics>>) -> (StatusCode, String) 
     }
 }
 
-async fn main_loop(metrics: Arc<Metrics>) -> Result<(), AlienError> {
-    let one_sec = tokio::time::Duration::from_secs(1);
-    let sleep_interval = tokio::time::Duration::from_secs(15);
-
-    let mut alien_client = AlienClient::new().await?;
-
-    loop {
-        metrics.scrape_counter.inc();
-
-        let device_info = alien_client.get_info().await;
-
-        if let Ok(device_info) = device_info {
-            alien_client.record_metrics(&metrics, device_info)?;
-            tokio::time::sleep(sleep_interval).await
-        } else {
-            alien_client.re_login().await?;
-            tokio::time::sleep(one_sec).await
-        }
-    }
-}
-
 async fn shutdown_signal() {
     let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
+        ctrl_c().await.expect("failed to install Ctrl+C handler");
     };
 
     #[cfg(unix)]
     let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
+        signal(SignalKind::terminate())
             .expect("failed to install signal handler")
             .recv()
             .await;
@@ -55,19 +60,19 @@ async fn shutdown_signal() {
     #[cfg(not(unix))]
     let terminate = std::future::pending::<()>();
 
-    tokio::select! {
+    select! {
         _ = ctrl_c => {},
         _ = terminate => {},
     }
 
-    println!("signal received, starting graceful shutdown");
+    println!("\nDEBUG: signal received, starting graceful shutdown");
 }
 
 #[tokio::main]
 async fn main() {
     let metrics = Arc::new(Metrics::new().unwrap());
     let addr = "0.0.0.0:9898".parse().unwrap();
-    println!("Listening on http://{}", addr);
+    println!("Listening on http://{}/metrics", addr);
 
     let app = Router::new()
         .route("/metrics", get(serve_req))
@@ -76,7 +81,7 @@ async fn main() {
         .serve(app.into_make_service())
         .with_graceful_shutdown(shutdown_signal());
 
-    tokio::select! {
+    select! {
         res = serve_future => {
             if res.is_err() {
                 eprintln!("ERROR: Metrics endpoint serve failure: {:?}", res);
